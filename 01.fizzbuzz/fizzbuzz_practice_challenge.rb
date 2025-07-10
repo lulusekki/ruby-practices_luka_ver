3f984f92126e3ab62cecb154db2d519a5fa8e425
LUKA SEKINE <luka.sekine@gmail.com>
2025-07-11 04:59:08 +0900

    2回目の提出
    ## やった(修正した)こと
    - 配列をベタがきではなくrange(範囲オブジェクト)に変更
    - if文の優先度を変更
     - 15倍数なら"FizzBuzz"という表示が正解なので、`num % 15 == 0`を一番最初に処理させる

--- 01.fizzbuzz/fizzbuzz_practice_challenge.rb
+++ 01.fizzbuzz/fizzbuzz_practice_challenge.rb
@@ -1,14 +1,13 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
 # お題：3の倍数のときは数の代わりに｢Fizz｣と、5の倍数のときは｢Buzz｣とプリントし、3と5両方の倍数の場合には｢FizzBuzz｣と表示
 # メモ：もし、3で割り切れたら(0)ならfizz、5で割り切れたら(0)ならbuzz、3と5両方の倍数(LCM15)ならfizzbuzz。それ以外なら配列内の数字を表示(puts)
-numbers.each do | num |
-  if num % 3 == 0
-    puts "Fizz"
+
+(1..20).each do | num |
+  if num % 15 == 0
+    puts "FizzBuzz"
   elsif num % 5 == 0
     puts "Buzz"
-  elsif num % 15 == 0
-    puts "FizzBuzz"
+  elsif num % 3 == 0
+    puts "Fizz"
   else puts num
   end
 end
