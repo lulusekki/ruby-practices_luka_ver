1e899090cb762b1ebcb891c9e6acecc128d6e6c2
LUKA SEKINE <luka.sekine@gmail.com>
2025-08-02 16:45:58 +0900

    入力された投球内容を元に10フレームに整える(分ける)

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,5 +3,33 @@
 # 前提処理
 X = "X"
 scores = ARGV[0].split(",")
+frames = []
+process_frames = []
 
-p scores
+# 引数の前処理：引数(投球データ)をフレームに当てはめる。10フレームもしっかり処理する
+# そのためには、格納するインデックス番号が0〜8なら、今の処理通りに追加する、9以上がある場合は全て1つのフレームとして処理する
+scores.each do |score|
+  if frames.size < 9
+    # 1〜9フレームの処理
+    if score == X
+      frames << [10]
+    else
+      process_frames << score.to_i
+      if process_frames.size == 2
+        frames << process_frames
+        process_frames = []
+      end
+    end
+  else
+    # 10フレーム目：残りのすべてをまとめて
+    process_frames << if score == X
+      10
+    else
+      score.to_i
+    end
+  end
+end
+
+frames << process_frames unless process_frames.empty?
+
+p frames
