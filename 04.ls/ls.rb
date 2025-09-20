74d1dbb318edb2024d73ff4036acd58dfdc900d5
LUKA SEKINE <luka.sekine@gmail.com>
2025-09-20 18:21:07 +0900

    1回目の提出

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,26 +1,43 @@
-path = "/Users/lukasekine/development"
-files_in_directory = Array.new(Dir.entries(path)).sort
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-column_num = 3
-remainder_num = files_in_directory.size.divmod(column_num)
+path = '/Users/lukasekine/development'
+files_in_directory = Array.new(Dir.children(path)).reject { |i| i == '.DS_Store' }.sort
+order_column_num = 3
 
-add_array_num = (column_num - remainder_num[1]) % column_num
+if files_in_directory.empty?
+  exit 0
+end
 
-add_array_element = [" "] * add_array_num
+remainder_num = files_in_directory.size.divmod(order_column_num)
+
+add_array_num = (order_column_num - remainder_num[1]) % order_column_num
+
+add_array_element = [' '] * add_array_num
 
 new_files_in_directory = (files_in_directory << add_array_element).flatten
 
-two_dimensional_array_num = new_files_in_directory.size / column_num
+two_dimensional_array_num = new_files_in_directory.size / order_column_num
 
-two_dimensional_array = new_files_in_directory.each_slice(two_dimensional_array_num).to_a
+def slice_to_array(array, rows)
+  array.each_slice(rows).to_a
+end
+
+two_dimensional_array = slice_to_array(new_files_in_directory, two_dimensional_array_num)
 
 completed_array = two_dimensional_array.transpose
 
-array_col_lengths = (files_in_directory.map { |item| item.length }).max
+file_name_max = files_in_directory.map(&:length).max
 
-completed_array.each do |item|
-  item.each do |row_itme|
-    print row_itme.to_s.ljust(array_col_lengths + 2," ")
+def output(completed_array, file_name_max)
+  add_padding_string = 2
+
+  completed_array.each do |item|
+    item.each do |row_itme|
+      print row_itme.to_s.ljust(file_name_max + add_padding_string, ' ')
+    end
+    puts
   end
-  puts
 end
+
+output(completed_array, file_name_max)
