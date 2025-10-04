395ee3f02fdf166aebee4be86c6d6bc5ba892415
LUKA SEKINE <luka.sekine@gmail.com>
2025-10-04 11:37:26 +0900

    定数の設定

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+ORDER_COLUM_NUM = 3
+ADD_PADDING_STRING = 2
+
 def main
   files_in_directory = Dir.glob("*")
-  order_column_num = 3
   
   return 0 if files_in_directory.empty?
   
-  remainder_num = files_in_directory.size.divmod(order_column_num)
-  add_array_num = (order_column_num - remainder_num[1]) % order_column_num
+  remainder_num = files_in_directory.size.divmod(ORDER_COLUM_NUM)
+  add_array_num = (ORDER_COLUM_NUM - remainder_num[1]) % ORDER_COLUM_NUM
   add_array_element = [' '] * add_array_num  
   new_files_in_directory = (files_in_directory << add_array_element).flatten
-  two_dimensional_array_num = new_files_in_directory.size / order_column_num
+  two_dimensional_array_num = new_files_in_directory.size / ORDER_COLUM_NUM
   two_dimensional_array = new_files_in_directory.each_slice(two_dimensional_array_num).to_a
   completed_array = two_dimensional_array.transpose
   file_name_max = files_in_directory.map(&:length).max  
@@ -19,11 +21,10 @@ def main
 end
 
 def output(completed_array, file_name_max)
-  add_padding_string = 2
 
   completed_array.each do |item|
     item.each do |row_itme|
-      print row_itme.to_s.ljust(file_name_max + add_padding_string, ' ')
+      print row_itme.to_s.ljust(file_name_max + ADD_PADDING_STRING, ' ')
     end
     puts
   end
