4c2e411e79eb64fb6aa95b0f7d51b75d1aff5dc6
LUKA SEKINE <luka.sekine@gmail.com>
2025-10-05 15:50:55 +0900

    変数名の変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,17 +2,17 @@
 # frozen_string_literal: true
 
 ORDER_COLUMN = 3
-PADDING = 2
+BLANK = 2
 
 def main
   files = Dir.glob('*')
 
   remainder = files.size % ORDER_COLUMN
-  blank_count = (ORDER_COLUMN - remainder) % ORDER_COLUMN
-  blanks = [''] * blank_count
-  blanked_files = files + blanks
-  rows_count = blanked_files.size / ORDER_COLUMN
-  files_grid = blanked_files.each_slice(rows_count).to_a
+  padding_count = (ORDER_COLUMN - remainder) % ORDER_COLUMN
+  paddings = [''] * padding_count
+  padded_files = files + paddings
+  rows_count = padded_files.size / ORDER_COLUMN
+  files_grid = padded_files.each_slice(rows_count).to_a
   display_files = files_grid.transpose
   column_width = files.map(&:length).max
   output(display_files, column_width)
