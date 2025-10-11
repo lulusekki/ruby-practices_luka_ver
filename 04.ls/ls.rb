1b587cab197be00775f43d1e102fefbfed14e971
LUKA SEKINE <luka.sekine@gmail.com>
2025-10-11 15:26:10 +0900

    1回目の提出

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,15 +5,20 @@ COLUMNS = 3
 BLANK = 2
 
 def main
-  files = Dir.glob('*')
+  files = ARGV.include?('-a') ? Dir.entries('.').sort : Dir.glob('*')
+  file_grid, column_width = build_file_grid(files, COLUMNS)
+  output(file_grid, column_width)
+end
 
-  remainder = files.size % COLUMNS
-  padding_count = (COLUMNS - remainder) % COLUMNS
+def build_file_grid(files, columns)
+  remainder = files.size % columns
+  padding_count = (columns - remainder) % columns
   padded_files = files + Array.new(padding_count, '')
-  row_count = padded_files.size / COLUMNS
+  row_count = padded_files.size / columns
   file_grid = padded_files.each_slice(row_count).to_a.transpose
   column_width = files.map(&:length).max
-  output(file_grid, column_width)
+
+  [file_grid, column_width]
 end
 
 def output(file_grid, column_width)
