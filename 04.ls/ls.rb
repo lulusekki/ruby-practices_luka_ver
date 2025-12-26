fe8d1fd71dc69178acc6555b660cb09dd656e6de
LUKA SEKINE <luka.sekine@gmail.com>
2025-12-27 06:38:06 +0900

    rubocopの修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -42,32 +42,31 @@ class LongOption
   def output(files)
     @stats = files.map { |file| [file, File.stat(file)] }
 
-    hard_link_width = @stats.map { |_file, stat| stat.nlink.to_s.length }.max
-
-    file_size_width = @stats.map { |_file, stat| stat.size.to_s.length }.max
-    last_modified_month_width = @stats.map { |_file, stat| stat.mtime.strftime('%-m月').length }.max
+    hard_link_width = @stats.map { |file, stat| stat.nlink.to_s.length }.max
+    file_size_width = @stats.map { |file, stat| stat.size.to_s.length }.max
+    last_modified_month_width = @stats.map { |file, stat| stat.mtime.strftime('%-m月').length }.max
 
     puts "total #{total}"
 
     # 設計修正メモ①：横にループさせながら、各行のデータを収集
     # この段段階では.rjustとかしない
 
-    @stats.map do |file, stat|
+    rows = @stats.map do |file,stat|
       [
-        permission(stat),
-        hard_link(stat).to_s.rjust(hard_link_width),
-        owner_name(stat),
-        group_name(stat),
-        file_size(stat).to_s.rjust(file_size_width),
-        last_modified_month(stat).to_s.rjust(last_modified_month_width),
-        last_modified_day(stat),
-        last_modified_hour_minute(stat),
-        file
-      ].join(' ')
+    permission(stat),
+    hard_link(stat).to_s.rjust(hard_link_width),
+    owner_name(stat),
+    group_name(stat),
+    file_size(stat).to_s.rjust(file_size_width),
+    last_modified_month(stat).to_s.rjust(last_modified_month_width),
+    last_modified_day(stat),
+    last_modified_hour_minute(stat),
+    file
+  ].join(' ')
     end
 
     # 設計修正メモ②：列の幅を求める必要があれば、その列だけ最大の幅を求める
-
+    
     # hogeから最大の幅を取得
 
     # hard_link_width = stats.map { |stat| stat.nlink.to_s.length }.max
@@ -84,6 +83,7 @@ class LongOption
     # hoge.each do
     #   puts ...
     # end
+    
   end
 
   private
