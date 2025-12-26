80fdf402882c8f26506467e32854b49d361502ea
LUKA SEKINE <luka.sekine@gmail.com>
2025-12-27 06:32:51 +0900

    statを使い回す方式に変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -40,94 +40,117 @@ end
 
 class LongOption
   def output(files)
-    stats = files.map { |file| File.stat(file) }
+    @stats = files.map { |file| [file, File.stat(file)] }
 
-    hard_link_width = stats.map { |stat| stat.nlink.to_s.length }.max
-    file_size_width = stats.map { |stat| stat.size.to_s.length }.max
-    last_modified_month_width = stats.map { |stat| stat.mtime.strftime('%-m月').length }.max
+    hard_link_width = @stats.map { |_file, stat| stat.nlink.to_s.length }.max
 
-    puts "total #{total(files)}"
+    file_size_width = @stats.map { |_file, stat| stat.size.to_s.length }.max
+    last_modified_month_width = @stats.map { |_file, stat| stat.mtime.strftime('%-m月').length }.max
 
-    files.each do |file|
-      puts [
-        permission(file),
-        hard_link(file).to_s.rjust(hard_link_width),
-        owner_name(file),
-        group_name(file),
-        file_size(file).to_s.rjust(file_size_width),
-        last_modified_month(file).to_s.rjust(last_modified_month_width),
-        last_modified_day(file),
-        last_modified_hour_minute(file),
+    puts "total #{total}"
+
+    # 設計修正メモ①：横にループさせながら、各行のデータを収集
+    # この段段階では.rjustとかしない
+
+    @stats.map do |file, stat|
+      [
+        permission(stat),
+        hard_link(stat).to_s.rjust(hard_link_width),
+        owner_name(stat),
+        group_name(stat),
+        file_size(stat).to_s.rjust(file_size_width),
+        last_modified_month(stat).to_s.rjust(last_modified_month_width),
+        last_modified_day(stat),
+        last_modified_hour_minute(stat),
         file
       ].join(' ')
     end
+
+    # 設計修正メモ②：列の幅を求める必要があれば、その列だけ最大の幅を求める
+
+    # hogeから最大の幅を取得
+
+    # hard_link_width = stats.map { |stat| stat.nlink.to_s.length }.max
+
+    # file_size_width = stats.map { |stat| file_size.to_s.length }.max
+
+    # last_modified_month_width = stats.map { |stat| stat.mtime.strftime('%-m月').length }.max
+
+    # totalを計算して出力
+    puts total
+
+    # 設計修正メモ③：集めたデータをきれいに整形して出力する
+    # hogeをきれいに整形して出力する
+    # hoge.each do
+    #   puts ...
+    # end
   end
 
   private
 
-  def total(files)
-    files.sum { |file| File.stat(file).blocks }
+  def total
+    @stats.sum { |stat| stat[1].blocks }
   end
 
-  def permission(file)
+  def permission(stat)
     [
-      file_type(file),
-      owner(file),
-      group(file),
-      other_group(file)
+      file_type(stat),
+      owner(stat),
+      group(stat),
+      other_group(stat)
     ].join
   end
 
-  def hard_link(file)
-    File.stat(file).nlink
+  def hard_link(stat)
+    stat.nlink
   end
 
-  def owner_name(file)
-    Etc.getpwuid(File.stat(file).uid).name
+  def owner_name(stat)
+    Etc.getpwuid(stat.uid).name
   end
 
-  def group_name(file)
-    Etc.getgrgid(File.stat(file).gid).name
+  def group_name(stat)
+    Etc.getgrgid(stat.gid).name
   end
 
-  def file_size(file)
-    File.stat(file).size
+  def file_size(stat)
+    stat.size
   end
 
-  def last_modified_month(file)
-    File.mtime(file).strftime('%-m月')
+  def last_modified_month(stat)
+    stat.mtime.strftime('%-m月')
   end
 
-  def last_modified_day(file)
-    File.mtime(file).strftime('%d')
+  def last_modified_day(stat)
+    stat.mtime.strftime('%d')
   end
 
-  def last_modified_hour_minute(file)
-    File.mtime(file).strftime('%H:%M')
+  def last_modified_hour_minute(stat)
+    stat.mtime.strftime('%H:%M')
   end
 
-  def file_mode(file)
-    File.stat(file).mode.to_s(8)
+  def file_mode(stat)
+    stat.mode.to_s(8)
   end
 
-  def file_type(file)
-    FILE_TYPE.fetch(file_mode(file)[0..1])
+  def file_type(stat)
+    FILE_TYPE.fetch(file_mode(stat)[0..1])
   end
 
-  def owner(file)
-    mode = file_mode(file)[-3]
+  def owner(stat)
+    mode = file_mode(stat)[-3]
     zero_padding_mode = mode.to_i.to_s(2).rjust(3, '0')
     PERMISSION_MODE.fetch(zero_padding_mode)
   end
 
-  def group(file)
-    mode = file_mode(file)[-2]
+  def group(stat)
+    mode = file_mode(stat)[-2]
     zero_padding_mode = mode.to_i.to_s(2).rjust(3, '0')
     PERMISSION_MODE.fetch(zero_padding_mode)
   end
 
-  def other_group(file)
-    mode = file_mode(file)[-1]
+  def other_group(stat)
+    mode = file_mode(stat)[-1]
     zero_padding_mode = mode.to_i.to_s(2).rjust(3, '0')
     PERMISSION_MODE.fetch(zero_padding_mode)
   end
