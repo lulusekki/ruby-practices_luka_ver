220580aa87ba5d2d75a7f6b26c8de4798ef930a3
LUKA SEKINE <luka.sekine@gmail.com>
2025-12-27 07:19:47 +0900

    行データと縦幅を求めるロジックの修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -42,48 +42,56 @@ class LongOption
   def output(files)
     @stats = files.map { |file| [file, File.stat(file)] }
 
-    hard_link_width = @stats.map { |file, stat| stat.nlink.to_s.length }.max
-    file_size_width = @stats.map { |file, stat| stat.size.to_s.length }.max
-    last_modified_month_width = @stats.map { |file, stat| stat.mtime.strftime('%-m月').length }.max
-
     puts "total #{total}"
 
     # 設計修正メモ①：横にループさせながら、各行のデータを収集
     # この段段階では.rjustとかしない
 
-    rows = @stats.map do |file,stat|
-      [
-    permission(stat),
-    hard_link(stat).to_s.rjust(hard_link_width),
-    owner_name(stat),
-    group_name(stat),
-    file_size(stat).to_s.rjust(file_size_width),
-    last_modified_month(stat).to_s.rjust(last_modified_month_width),
-    last_modified_day(stat),
-    last_modified_hour_minute(stat),
-    file
-  ].join(' ')
+    rows = []
+
+    @stats.each do |file, stat|
+      rows << {
+        permission: permission(stat),
+        hard_link: hard_link(stat),
+        owner_name: owner_name(stat),
+        group_name: group_name(stat),
+        file_size: file_size(stat),
+        last_modified_month: last_modified_month(stat),
+        last_modified_day: last_modified_day(stat),
+        last_modified_hour_minute: last_modified_hour_minute(stat),
+        file: file
+      }
     end
 
     # 設計修正メモ②：列の幅を求める必要があれば、その列だけ最大の幅を求める
-    
     # hogeから最大の幅を取得
-
-    # hard_link_width = stats.map { |stat| stat.nlink.to_s.length }.max
-
-    # file_size_width = stats.map { |stat| file_size.to_s.length }.max
-
-    # last_modified_month_width = stats.map { |stat| stat.mtime.strftime('%-m月').length }.max
+    hard_link_width = @stats.map { |_file, stat| stat.nlink.to_s.length }.max
+    file_size_width = @stats.map { |_file, stat| stat.size.to_s.length }.max
+    last_modified_month_width = @stats.map { |_file, stat| stat.mtime.strftime('%-m月').length }.max
 
     # totalを計算して出力
     puts total
 
     # 設計修正メモ③：集めたデータをきれいに整形して出力する
+
+    rows.each do |row|
+      puts [
+        row[:permission],
+        row[:hard_link].to_s.rjust(hard_link_width),
+        row[:owner_name],
+        row[:group_name],
+        row[:file_size].to_s.rjust(file_size_width),
+        row[:last_modified_month].to_s.rjust(last_modified_month_width),
+        row[:last_modified_day],
+        row[:last_modified_hour_minute],
+        row[:file]
+      ].join(' ')
+    end
+
     # hogeをきれいに整形して出力する
     # hoge.each do
     #   puts ...
     # end
-    
   end
 
   private
