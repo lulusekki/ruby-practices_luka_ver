801e249c9924ac085f17b800a62e3367396588ed
LUKA SEKINE <luka.sekine@gmail.com>
2026-02-05 17:43:11 +0900

    変数名などの微修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,42 +6,42 @@ require 'optparse'
 WIDTH = 8
 
 def main
-  keys = parse_options
+  options = parse_options
   totals = Hash.new(0)
 
   if ARGV.empty?
     content = $stdin.read
     row_informations = file_information(content)
-    print_counts(keys, row_informations, '')
+    print_counts(options, row_informations, '')
   else
     ARGV.each do |file_name|
       content = File.read(file_name)
       row_informations = file_information(content)
 
-      keys.each do |key|
-        totals[key] += row_informations[key]
+      options.each do |option|
+        totals[option] += row_informations[option]
       end
 
-      print_counts(keys, row_informations, file_name)
+      print_counts(options, row_informations, file_name)
     end
 
     file_size = 2
-    print_counts(keys, totals, 'total') if ARGV.size >= file_size
+    print_counts(options, totals, 'total') if ARGV.size >= file_size
   end
 end
 
 def parse_options
-  keys = []
+  options = []
 
   OptionParser.new do |opt|
-    opt.on('-l') { |_key| keys << :line }
-    opt.on('-w') { |_key| keys << :word }
-    opt.on('-c') { |_key| keys << :byte }
+    opt.on('-l') { |_option| options << :line }
+    opt.on('-w') { |_option| options << :word }
+    opt.on('-c') { |_option| options << :byte }
 
     opt.parse!(ARGV)
   end
 
-  keys.empty? ? %i[line word byte] : keys
+  options.empty? ? %i[line word byte] : options
 end
 
 def file_information(content)
@@ -52,12 +52,12 @@ def file_information(content)
   }
 end
 
-def print_counts(keys, row_informations, label)
-  values = keys.map do |key|
-    row_informations[key].to_s.rjust(WIDTH)
+def print_counts(options, row_informations, label)
+  output_content = options.map do |option|
+    row_informations[option].to_s.rjust(WIDTH)
   end
 
-  puts "#{values.join(' ')} #{label}"
+  puts "#{output_content.join(' ')} #{label}"
 end
 
 main
